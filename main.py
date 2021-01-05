546a408a5b7a32e83b6239b8f9562878cd6a1526
Anshrk <Anshrk.13@gmail.com>
2021-01-06 00:08:22 +0530

    changes were made

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import random
 
 bot = commands.Bot(command_prefix='>')
 
+# Opening message
 @bot.event
 async def on_ready():
     await bot.change_presence(status=discord.Status.online, activity=discord.Game('programming my self, use ">help" to summon"'))
@@ -13,12 +14,15 @@ async def on_ready():
     print("Bot is live!")
 
 
+# alert new joiners
 @bot.event
 async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(
         f"Hi {member.name}, welcome to Programmer's Cafe :>")
 
+
+# command to post a youtubevideo
 @bot.command(aliases=["yt"])
 async def youtube(ctx, *, arg):
     """Search YouTube"""
@@ -35,6 +39,35 @@ async def youtube(ctx, *, arg):
         await ctx.send(f"> Here is your result for: {query}\n{payload}")
 
 
+# Command to display the latency of the bot.
+@bot.command()
+async def ping(ctx):
+    """Display the latency, with a pong ofcource."""
+    await ctx.send(f"Pong! {round(bot.latency * 1000)}")
+
+# Clear Messages from the bot.
+@bot.command()
+async def clear(ctx, amount=10):
+    """Clear Messages."""
+    await ctx.channel.purge(limit=amount+1)
+
+
+# command to Kick and Ban.
+@bot.command()
+async def kick(ctx, member : discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+@bot.command()
+async def ban(ctx, member: discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+
+
+# @bot.command()
+# async def poll(ctx, *, message):
+#     """Creates a Poll."""
+#     embd = discord.Embded(title=f"Poll created by {ctx.author}", discription=f"{message}")
+#     msg = await ctx.channel.send(embded=embd)
+#     await msg.add_reaction(":thumbsup:")
+#     await msg.add_reaction(":down:")
 
 
 
