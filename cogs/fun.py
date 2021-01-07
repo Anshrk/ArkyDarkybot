50b373231c507e6c23d209ebadc097d62c1e5165
Anshrk <Anshrk.13@gmail.com>
2021-01-07 13:18:20 +0530

    added yotube command

--- cogs/fun.py
+++ cogs/fun.py
@@ -7,8 +7,20 @@ class FunCommands(commands.Cog):
         self.client = client
 
 
-
-
+    @bot.command(aliases=["yt"])
+    async def youtube(self, ctx, *, arg):
+        """Search YouTube"""
+        query = str(arg)
+        # print("query: ", query)
+        url = "https://www.youtube.com/results?search_query="
+        with requests.get(url + query) as response:
+            # regex = '/watch\?v\=[a-zA-z0-9/_/-/*]+'
+            regex = '/watch\?v\=(.*?)\"'
+            # regex = r'/watch\?v=[a-zA-Z0-9]+'
+            match = re.findall(regex, response.text)[0]
+            payload = "https://www.youtube.com/watch?v=" + match
+            # print(payload)
+            await ctx.send(f"> Here is your result for: {query}\n{payload}")
 
 def setup(client):
     client.add_cog(FunCommands(client))
