3df28ed240ec68bba5783312542f8efe423c2ae3
Anshrk <Anshrk.13@gmail.com>
2021-01-07 13:44:40 +0530

    added changing messages

--- cogs/startup.py
+++ cogs/startup.py
@@ -9,7 +9,6 @@ class StartupCommands(commands.Cog):
     # on startup
     @commands.Cog.listener()
     async def on_ready(self):
-        await client.change_presence(status=discord.Status.idle, activity=discord.Game("Hello There!"))
         print("The Bot is ready to take over the wor- serve you!!!")
 
 
@@ -18,7 +17,18 @@ class StartupCommands(commands.Cog):
     async def ping(self, ctx):
         await ctx.send(f"Pong! {round(self.client.latency * 1000)}")
 
-
+    @tasks.loop(seconds=10)
+    async def(self, seconds=10):
+        await client.changepresence(
+            activity=discord.Game(
+                cycle(
+                    ["""never gonna give you up
+                     Never gonna let you down
+                     Never gonna run around
+                     And Hurt you""".split(" ")]
+                    )
+                )
+            )
 
 def setup(client):
     client.add_cog(StartupCommands(client))
